5fa10f60c6a0988b60f0a737f5406aa771abbddd
leyan95 <18231926271@163.com>
2019-10-21 20:58:52 +0800

    update.
    🥧wow

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,63 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
+
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    activityRecords: [],
+    page: 1
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
+  onLoad: function (options) {
+    this.refresh();
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+
+  onPullDownRefresh: function () {
+    this.refresh();
+  },
+
+  onReachBottom: function () {
+    this.loadMore();
+  },
+
+  refresh: function () {
+    wx.showNavigationBarLoading();
+    var that = this;
+    wx.request({
+      url: 'https://www.wetest.com/home/list_record?page=1',
+      method: "GET",
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (response) {
+        that.setData({
+          activityRecords: response.data.object,
+          page: 1
+        });
+        console.log(that.data.activityRecords);
+        wx.hideNavigationBarLoading();
+        wx.stopPullDownRefresh();
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    });
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  
+  loadMore: function () {
+    var that = this;
+    wx.showLoading({
+      title: '玩命加载中',
+    });
+    wx.request({
+      url: 'https://www.wetest.com/home/list_record?page=' + this.data.page + 1,
+      method: "GET",
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (response) {
+        const oldData = that.data.activityRecords;
+        that.setData({
+          activityRecords: oldData.concat(response.data.object),
+          page: that.data.page + 1
+        })
+        console.log(that.data.activityRecords);
+        wx.hideLoading();
+      }
+    });
   }
 })
