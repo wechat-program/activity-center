9ad81aff6a0c65e6c064a91a2686178214ba087b
leyan95 <18231926271@163.com>
2019-10-23 13:29:54 +0800

    🎨 udpate detail page.
    whoa

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,12 +1,13 @@
 var util = require('../../utils/util.js');
 Page({
-  data: {},
+    data: {},
 
-  onLoad: function(options) {
-    console.log(options)
-    // 赋值
-    this.setData({
-      detail: util.getDetailByUuid(options.uuid)
-    })
-  }
+    onLoad: function(options) {
+        // 赋值
+        this.setData({
+            detail: util.getDetailByUuid(options.uuid)
+        })
+        console.log(this.data.detail.title);
+        console.log(this.data.detail.images);
+    }
 });
